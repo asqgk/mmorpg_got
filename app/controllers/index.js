f7c4a09ca0bcb392e5b8f26b22ba9f705165ba38
Francisco dos Passos <chico.81@hotmail.com>
2021-06-28 13:19:24 -0300

    front-end changes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.index = function(application, req, res){
-    res.render('index');
+    res.render('index', {validacao: {}, dadosForm: {} });
+}
+
+module.exports.autenticar = function(application, req, res){
+    
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'O usuário não pode ser vazio').notEmpty();
+    req.assert('senha', 'A senha não pode ser vazia').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('index', {validacao: erros, dadosForm: dadosForm });
+        return;
+    }
+
+    res.send('ok');
 }
